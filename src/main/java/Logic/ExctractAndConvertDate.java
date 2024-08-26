b92cc07005c93de4fc82ac69d98ed04e19297323
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-08-25 20:48:37 -0500

    [docs] add comments and identation to make clearer of reading

--- src/main/java/Logic/ExctractAndConvertDate.java
+++ src/main/java/Logic/ExctractAndConvertDate.java
@@ -14,58 +14,58 @@ import java.util.stream.Stream;
  *
  * @author soyky
  */
-
 //format YYYY-MM-DD 
 public class ExctractAndConvertDate {
-    
-    
+
 //    YYYY-MM-DD-HH-MM
-    public static List<Integer> extractDateTime(String unformattedDateTime  ,String separator){
+    public static List<Integer> extractDateTime(String unformattedDateTime, String separator) {
         // 9 is the last index from date according to next format YYYY-MM-DD-HH-MM 
         int endIndexDate = 9;
-        String unfformattedDate = unformattedDateTime.substring(0, endIndexDate + 1);
-        List<Integer> date = ExctractAndConvertDate.extractDate(unfformattedDate, separator);
+        String unformattedDate = unformattedDateTime.substring(0, endIndexDate + 1);
+        List<Integer> date = ExctractAndConvertDate.extractDate(unformattedDate, separator);
         //unformattedTime start from index endIndexDate + 2 beacuse have to sum the separator and real "startIndexTime"
         String unformattedTime = unformattedDateTime.substring(endIndexDate + 2);
-        List<Integer> time = ExctractAndConvertDate.extractTime(unfformattedDate, separator);
+        List<Integer> time = ExctractAndConvertDate.extractTime(unformattedTime, separator);
         return Stream.concat(date.stream(), time.stream()).toList();
-        
-        
+
     }
-    
-    public static List<Integer> extractDate(String unformattedDate ,String separator){
+
+    public static List<Integer> extractDate(String unformattedDate, String separator) {
         List<String> datePartsAsStrings = Arrays.asList(unformattedDate.split(separator));
-        Stream<Integer> dateParts =  datePartsAsStrings.stream().map((curString) -> Integer.parseInt(curString));
-        
-        return  dateParts.toList();
-        
+        Stream<Integer> dateParts = datePartsAsStrings.stream().map((curString) -> Integer.parseInt(curString));
+
+        return dateParts.toList();
+
     }
 //    HH-MM
-    public static List<Integer> extractTime(String unformattedTime , String separator ){
-        List<String> timePartsAsStrings =  Arrays.asList(unformattedTime.split(separator));
+
+    public static List<Integer> extractTime(String unformattedTime, String separator) {
+        List<String> timePartsAsStrings = Arrays.asList(unformattedTime.split(separator));
         Stream<Integer> timeParts = timePartsAsStrings.stream().map((curTimePart) -> Integer.parseInt(curTimePart));
         return timeParts.toList();
-        
+
     }
-    
-    
-    
-    public static Calendar convertToDate(List<Integer> dateParts){
+
+    public static Calendar convertToDate(List<Integer> dateParts) {
         return new GregorianCalendar(
                 dateParts.get(0),
-                dateParts.get(1),
+                // is less 1 beacause Calendar represent the months in range 0 -11
+                dateParts.get(1) - 1,
                 dateParts.get(2)
         );
     }
-    
-    public static Calendar convertToDateTime(List<Integer> datetimeParts){
-        return new GregorianCalendar(
+
+    public static Calendar convertToDateTime(List<Integer> datetimeParts) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(
                 datetimeParts.get(0),
-                datetimeParts.get(1),
+                // is less 1 beacause Calendar represent the months in range 0 -11
+                datetimeParts.get(1) - 1,
                 datetimeParts.get(2),
-                datetimeParts.get(4),
-                datetimeParts.get(5)
+                datetimeParts.get(3),
+                datetimeParts.get(4)
         );
+        return calendar;
     }
-    
+
 }
