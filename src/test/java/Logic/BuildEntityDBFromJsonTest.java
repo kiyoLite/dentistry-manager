02ece3ab7178e2bfbac73c72d9307ea5f4786f0d
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-08-25 20:33:43 -0500

    [test] create a test to checkout if shift is creating according to json

--- src/test/java/Logic/BuildEntityDBFromJsonTest.java
+++ src/test/java/Logic/BuildEntityDBFromJsonTest.java
@@ -6,6 +6,7 @@ package Logic;
 
 import Persistence.Entities.Patient;
 import Persistence.Entities.Person;
+import Persistence.Entities.Shift;
 import Persistence.Enums.InsuranceProvider;
 import java.util.Calendar;
 import org.json.JSONObject;
@@ -19,12 +20,12 @@ import org.junit.jupiter.api.Test;
  * @author soyky
  */
 public class BuildEntityDBFromJsonTest {
-    
+
     JSONObject json;
-    
+
     @BeforeEach
-    public void setUp(){
-         String jsonString =  "{"
+    public void setUp() {
+        String jsonString = "{"
                 + "\"dentist\": \"1\","
                 + "\"patientBirthDate\": \"2007-04-19\","
                 + "\"patientDisability\": \"\","
@@ -34,20 +35,19 @@ public class BuildEntityDBFromJsonTest {
                 + "\"patientLastName\": \"dev\","
                 + "\"shiftDate\": \"2024-08-25\","
                 + "\"shiftPrice\": \"70\","
-                + "\"shiftReason\": \"fill bad\","
+                + "\"shiftReason\": \"feel bad\","
                 + "\"shiftTime\": \"14:30\""
                 + "}";
-         json = new JSONObject(jsonString);
+        json = new JSONObject(jsonString);
     }
-    
+
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
         json = null;
     }
-    
-    
+
     @Test
-     public void createPersonPatient(){
+    public void createPersonPatient() {
         BuildEntityDBFromJson build = new BuildEntityDBFromJson();
         Person person = build.createPersonPatient(json);
         Assertions.assertNotNull(person);
@@ -57,16 +57,32 @@ public class BuildEntityDBFromJsonTest {
         Calendar expectedDate = Calendar.getInstance();
         expectedDate.set(2007, Calendar.APRIL, 19);
         Assertions.assertEquals(expectedDate.get(Calendar.YEAR), person.getBirthDate().get(Calendar.YEAR));
-        Assertions.assertEquals(expectedDate.get(Calendar.MONTH), person.getBirthDate().get(Calendar.MONTH),"was here !!!!");
+        Assertions.assertEquals(expectedDate.get(Calendar.MONTH), person.getBirthDate().get(Calendar.MONTH), "was here !!!!");
         Assertions.assertEquals(expectedDate.get(Calendar.DAY_OF_MONTH), person.getBirthDate().get(Calendar.DAY_OF_MONTH));
-     }
-     
-     @Test 
-     public void createPatient(){
-         Patient patient =  new BuildEntityDBFromJson().createPatient(json);
-         
-         Assertions.assertNotNull(patient);
+    }
+
+    @Test
+    public void createPatient() {
+        Patient patient = new BuildEntityDBFromJson().createPatient(json);
+
+        Assertions.assertNotNull(patient);
         Assertions.assertEquals(null, patient.getDisability());
         Assertions.assertEquals(InsuranceProvider.AETNA, patient.getInsuranceProvider());
-     }
+    }
+
+    @Test
+    public void createShift() {
+        Shift shift = new BuildEntityDBFromJson().createShift(json);
+
+        Assertions.assertNotNull(shift);
+        Assertions.assertEquals(70, shift.getPrice());
+        Assertions.assertEquals("feel bad", shift.getReason());
+        Calendar expectedScheduling = Calendar.getInstance();
+        expectedScheduling.set(2024, Calendar.AUGUST, 25, 14, 30);
+        Assertions.assertEquals(expectedScheduling.get(Calendar.YEAR), shift.getScheduling().get(Calendar.YEAR));
+        Assertions.assertEquals(expectedScheduling.get(Calendar.MONTH), shift.getScheduling().get(Calendar.MONTH));
+        Assertions.assertEquals(expectedScheduling.get(Calendar.DAY_OF_MONTH), shift.getScheduling().get(Calendar.DAY_OF_MONTH));
+        Assertions.assertEquals(expectedScheduling.get(Calendar.HOUR_OF_DAY), shift.getScheduling().get(Calendar.HOUR_OF_DAY));
+        Assertions.assertEquals(expectedScheduling.get(Calendar.MINUTE), shift.getScheduling().get(Calendar.MINUTE));
+    }
 }
