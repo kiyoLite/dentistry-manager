48a078e13c1ecdc9b4a62e0f1a145a65a73b6de7
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-08-29 20:19:32 -0500

    [feat] add the body to Sv for sending the json to frontend

--- src/main/java/Servelts/SvIdAndDentistName.java
+++ src/main/java/Servelts/SvIdAndDentistName.java
@@ -4,14 +4,17 @@
  */
 package Servelts;
 
+import Logic.JSONBuilder;
 import Persistence.DAO.Implementation.DentistDAOImp;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 /**
  *
@@ -19,7 +22,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "SvIdAndDentistName", urlPatterns = {"/SvIdAndDentistName"})
 public class SvIdAndDentistName extends HttpServlet {
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,7 +31,19 @@ public class SvIdAndDentistName extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        DentistDAOImp dentistDAO = new DentistDAOImp();
+        List<Object[]> dentistNameAndId = dentistDAO.getAllIdAndDentistName();
+        JSONBuilder jsonBuilder = new JSONBuilder();
+        String[] fieldsNames = new String[]{"Id,Name"};
+        String genericKey = "register";
+        JSONObject responseJSON = jsonBuilder.createFromListGenericsObjects(dentistNameAndId, fieldsNames, genericKey);
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseJSON.toString());
+
     }
+
     @Override
     public String getServletInfo() {
         return "Short description";
