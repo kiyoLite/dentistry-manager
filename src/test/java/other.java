4fb138b40af162b7d65b855746b34506c5a23602
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-08-30 22:52:20 -0500

    [test] create a method that just create a session beacuse make easier checkout if DB is populate correclty

--- src/test/java/other.java
+++ src/test/java/other.java
@@ -1,3 +1,8 @@
+
+import Persistence.HibernateUtil;
+import org.hibernate.Session;
+import org.junit.jupiter.api.Test;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,4 +14,8 @@
  */
 public class other {
     
+    @Test
+    public void createSession(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+    }
 }
