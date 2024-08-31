ddd1ca65457613c27c9713d940854c7a14b041bc
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-08-31 15:59:22 -0500

    [test] create test to checkout if createFromListGenericsObjects is working correctly if maiJson size is different to 0 and if every subJSON have 2 properties

--- src/test/java/Logic/JSONBuilderTest.java
+++ src/test/java/Logic/JSONBuilderTest.java
@@ -20,19 +20,34 @@ public class JSONBuilderTest {
         Dentist dentistDB = new Dentist(Specialization.MAXILLOFACIAL, scheduleDB, personDB);
         DentistDAOImp crudDentist = new DentistDAOImp();
         crudDentist.create(dentistDB);
-        
+
         List<Object[]> collectionObjects = crudDentist.getAllIdAndDentistName();
         Object[] GenericObject = collectionObjects.get(0);
         JSONBuilder builder = new JSONBuilder();
-        JSONObject responseJSON = builder.createFromGenericObject(GenericObject, new String[]{"id","name"});
-        Assertions.assertTrue(responseJSON.length() >= 1);
-        String unformattedId = responseJSON.optString("id") ;
+        JSONObject responseJSON = builder.createFromGenericObject(GenericObject, new String[]{"id", "name"});
+        Assertions.assertTrue(responseJSON.length() == 2);
+        String unformattedId = responseJSON.optString("id");
         long id = !unformattedId.equals("") ? Long.parseLong(unformattedId) : 0L;
         String name = responseJSON.optString("name");
-        Assertions.assertNotEquals(0 , id);
+        Assertions.assertNotEquals(0, id);
         Assertions.assertTrue(!"".equals(name));
-        
-        
-        
+
+    }
+
+    @Test
+    public void createFromListGenericsObjects() {
+        Schedule scheduleDB = new Schedule(Calendar.getInstance(), Calendar.getInstance());
+        Person personDB = new Person("name1", "name2", Calendar.getInstance(), "email");
+        Dentist dentistDB = new Dentist(Specialization.MAXILLOFACIAL, scheduleDB, personDB);
+        DentistDAOImp crudDentist = new DentistDAOImp();
+        crudDentist.create(dentistDB);
+
+        List<Object[]> collectionObjects = crudDentist.getAllIdAndDentistName();
+        JSONBuilder builder = new JSONBuilder();
+        String genericKeyForRegisters = "registers";
+        JSONObject responseJSON = builder.createFromListGenericsObjects(collectionObjects, new String[]{"id", "name"}, genericKeyForRegisters);
+        JSONObject subJSON = responseJSON.getJSONObject(genericKeyForRegisters + 1);
+        Assertions.assertTrue(subJSON.length() == 2);
+
     }
 }
