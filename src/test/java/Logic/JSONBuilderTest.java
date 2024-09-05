db291f44cecc0c91a918acdff08f90dd80ce1633
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-09-05 14:00:03 -0500

    [test] create a method to checkout if createFromShift method work correctly

--- src/test/java/Logic/JSONBuilderTest.java
+++ src/test/java/Logic/JSONBuilderTest.java
@@ -2,14 +2,18 @@ package Logic;
 
 import Persistence.DAO.Implementation.DentistDAOImp;
 import Persistence.Entities.Dentist;
+import Persistence.Entities.Patient;
 import Persistence.Entities.Person;
 import Persistence.Entities.Schedule;
+import Persistence.Entities.Shift;
+import Persistence.Enums.InsuranceProvider;
 import Persistence.Enums.Specialization;
 import java.util.Calendar;
 import java.util.List;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
 public class JSONBuilderTest {
 
@@ -49,5 +53,33 @@ public class JSONBuilderTest {
         JSONObject subJSON = responseJSON.getJSONObject(genericKeyForRegisters + 1);
         Assertions.assertTrue(subJSON.length() == 2);
 
+    }
+
+    @Test
+    public void createFromShift() {
+        Person personpatient = new Person("name1", "name2", Calendar.getInstance(), "email");
+        Person personDentist = new Person("name1", "name2", Calendar.getInstance(), "email");
+        Patient patient = new Patient("disability", InsuranceProvider.AETNA, personpatient);
+        Schedule schedule = new Schedule(Calendar.getInstance(), Calendar.getInstance());
+        Dentist dentist = new Dentist(Specialization.MAXILLOFACIAL, schedule, personDentist);
+        Shift shift = new Shift(patient, dentist, Calendar.getInstance(), "reason", 0);
+
+        String[] expectJSONKeys = new String[]{
+            "patientEps",
+            "dentistId",
+            "price",
+            "patientFirstName",
+            "scheduling",
+            "patientEmail",
+            "dentistName",
+            "patientBirthDate",
+            "shiftReason",
+            "patientDisability",
+            "patientLastName"};
+        JSONBuilder builder = new JSONBuilder();
+        JSONObject JSONFromShift = builder.createFromShift(shift);
+        Object[] JSONkeys = JSONFromShift.keySet().toArray();
+        Assertions.assertTrue(Arrays.equals(JSONkeys,expectJSONKeys));
+        
     }
 }
