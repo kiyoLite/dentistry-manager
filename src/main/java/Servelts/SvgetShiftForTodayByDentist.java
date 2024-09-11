4d1db177430d66ab432108c0de34b0b31a79b4c5
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-09-10 19:33:20 -0500

    [perf]remove unnecesary deprecation\

--- src/main/java/Servelts/SvgetShiftForTodayByDentist.java
+++ src/main/java/Servelts/SvgetShiftForTodayByDentist.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import Persistence.Entities.Shift;
-import java.net.http.HttpResponse;
 import org.json.JSONObject;
 
 /**
