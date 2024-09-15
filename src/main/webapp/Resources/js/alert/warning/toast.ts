e662a5bbf408b83248177b83ee874cb9b7a1c487
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-09-14 21:09:01 -0500

    [feat] create warning toast. to guide user what have to do

---
+++ src/main/webapp/Resources/js/alert/warning/toast.ts
@@ -0,0 +1,29 @@
+import { toastMessage, removeToast } from "../Error/toast.js";
+const createToastWarning = function (toastMessage: toastMessage) {
+    const toast = document.createElement("div");
+    toast.innerHTML = `
+        <div class="ContainerImage">
+            <img src="../images/warningIcon.svg" alt="" class="Image">
+        </div>
+        <div class="ContaienrText">
+            <p class="Title">warning</p>
+            <p class="Description">${toastMessage.description}</p>
+        </div>
+        `;
+    toast.classList.add("WarningToastContainer");
+    return toast;
+}
+
+const leftScreenToastWarning = function (toastMessage: toastMessage) {
+    const header = document.getElementById("ContainerHedaer");
+    const headerHeight = header?.offsetHeight ?? 0;
+    const toast = createToastWarning(toastMessage);
+    const Axes = header?.getBoundingClientRect();
+    toast.style.top = (Axes?.top ?? 0 + headerHeight) + "px";
+    toast.style.left = Axes?.left + "px";
+    document.body.insertAdjacentElement("afterbegin", toast);
+    const toastSecondDuration = 5000;
+    removeToast(toast, toastSecondDuration);
+}
+
+export { leftScreenToastWarning }
